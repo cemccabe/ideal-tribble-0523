7095023f7bc9adf39fa0a3c154a9f7fbb40a334d
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-01-02 11:18:24 -0700

    Create model test for #actors_by_age method

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Movie do
     it {should have_many :movie_actors}
     it {should have_many(:actors).through(:movie_actors)}
   end
+
+  describe 'instance methods' do
+    before :each do
+      @studio1 = Studio.create!(name: "Paramount Pictures", location: "Hollywood")
+      @studio2 = Studio.create!(name: "Universal Studios", location: "Los Angeles")
+      @movie1 = @studio1.movies.create!(title: "Top Gun", creation_year: 1986, genre: "Action")
+      @movie2 = @studio2.movies.create!(title: "Harry Potter", creation_year: 2001, genre: "Fantasy")
+      @movie3 = @studio1.movies.create!(title: "Dumb and Dumber", creation_year: 1994, genre: "Comedy")
+      @actor1 = @movie1.actors.create!(name: "Tom Cruise", age: 60)
+      @actor2 = @movie1.actors.create!(name: "Val Kilmer", age: 63)
+      @actor3 = @movie1.actors.create!(name: "Kelly McGillis", age: 65)
+    end
+
+    it 'returns all actors in a movie ordered by age (young to old' do
+      expect(@movie1.actors_by_age).to eq([@actor1, @actor2, @actor3])
+    end
+  end
 end
